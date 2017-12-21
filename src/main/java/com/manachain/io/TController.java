15fbbf0f3390f0203a1b0a21ed854c69b083625d
Alberto <xchao@Albertos-MBP.lan>
2017-12-21 22:37:18 +0800

    save ticker to db

--- src/main/java/com/manachain/io/TController.java
+++ src/main/java/com/manachain/io/TController.java
@@ -17,22 +17,6 @@ public class TController {
     @RequestMapping("/")
     public String root(){
 
-//        tTicker tt = new tTicker();
-//        tt.setCurrencyPair("BTC/USD");
-//        tt.setOpen(null);
-//        tt.setLast(new BigDecimal(16306.82));
-//        tt.setBid(new BigDecimal(16319.14));
-//        tt.setAsk(new BigDecimal(16319.14));
-//        tt.setHigh(new BigDecimal(17950.00));
-//        tt.setLow(new BigDecimal(15343.04));
-//        tt.setVolume(new BigDecimal(25948.23549891));
-//        tt.setQuoteVolume(null);
-//
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTimeInMillis(new Long("1513834285000"));
-//
-//        tt.setTimestamp(calendar.getTime());
-//        ttickerRepository.save(tt);
 
         return "root ok";
     }
--- src/main/java/com/manachain/io/tService.java
+++ src/main/java/com/manachain/io/tService.java
@@ -1,5 +1,7 @@
 package com.manachain.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
@@ -7,23 +9,49 @@ import org.knowm.xchange.bitstamp.BitstampExchange;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.service.marketdata.MarketDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class tService {
-    Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
 
-    MarketDataService marketDataService = bitstamp.getMarketDataService();
+    private static final Logger log = LoggerFactory.getLogger(tService.class);
 
-    public void getTicker() throws IOException{
-        generic(marketDataService);
+    @Autowired
+    private tTickerRepository ttickerRepository;
 
+
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
+
+    MarketDataService marketDataService = exchange.getMarketDataService();
+
+
+    private static void generic(MarketDataService marketDataService, tTickerRepository ttickerRepository) throws IOException {
+        Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
+        System.out.println(ticker.toString());
+
+        tTicker tt = new tTicker();
+        tt.setCurrencyPair(ticker.getCurrencyPair().toString());
+        tt.setOpen(ticker.getOpen());
+        tt.setLast(ticker.getLast());
+        tt.setBid(ticker.getBid());
+        tt.setAsk(ticker.getAsk());
+        tt.setHigh(ticker.getHigh());
+        tt.setLow(ticker.getLow());
+        tt.setVolume(ticker.getVolume());
+        tt.setQuoteVolume(ticker.getQuoteVolume());
+        tt.setTimestamp(ticker.getTimestamp());
+
+        ttickerRepository.save(tt);
     }
 
+    @Scheduled(fixedRate = 5000)
+    public void getTicker() throws IOException {
+        generic(marketDataService, ttickerRepository);
 
-    private static void generic(MarketDataService marketDataService) throws IOException {
-        Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_USDT);
-        System.out.println(ticker.toString());
     }
 
 }
